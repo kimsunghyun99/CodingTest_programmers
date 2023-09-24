ed066ed8ee56a30105715c02cd69918fd1dd6f4b
sunghyun kim <105155263+kimsunghyun99@users.noreply.github.com>
2023-09-24 20:06:26 +0900

    [level 2] Title: 올바른 괄호, Time: 18.45 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,31 +1,32 @@
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int a = 0;
-
-        // 문자열을 문자 배열로 변환
-        char[] chars = s.toCharArray();
-
-        // 괄호 짝 검사
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == '(') {
-                a += 1;
-            } else if (chars[i] == ')') {
-                a -= 1;
+        
+        
+       
+        Stack<Character> stack = new Stack<>();
+       
+        
+        for(char ch : s.toCharArray()) {
+            
+            if(ch =='(') {
+                stack.push(ch);
             }
-
-            // 괄호 짝이 맞지 않으면 false 반환
-            if (a < 0) {
-                answer = false;
-                break;
+            else if(ch ==')') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
             }
         }
+            
+        
+       
+            
+        
+   
 
-        // 모든 괄호 짝이 맞고, 첫 문자가 '('이며, 마지막 문자가 ')'인 경우에만 true 반환
-        if (a != 0 || chars[0] != '(' || chars[chars.length - 1] != ')') {
-            answer = false;
-        }
-
-        return answer;
+        return stack.isEmpty();
+         }
     }
-}
+    
