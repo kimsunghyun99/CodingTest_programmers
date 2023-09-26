c70d8615270c3429de676f989eba0ae2c6bd89ed
sunghyun kim <105155263+kimsunghyun99@users.noreply.github.com>
2023-09-27 00:36:53 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i= 0; i<arr.length; i++) {
+            if(arr[i] == 1 && i>=idx) {
+                return i;
+            } 
+            else {
+                answer = -1;
+                
+            }
+        }
+        return answer;
+    }
+}
