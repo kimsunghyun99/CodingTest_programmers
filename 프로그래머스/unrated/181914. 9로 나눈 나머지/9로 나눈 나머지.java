f1ba84bd43c4288c45f21f9e49a29caa4c566f76
sunghyun kim <105155263+kimsunghyun99@users.noreply.github.com>
2023-09-29 20:16:43 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        char c[] = number.toCharArray();
+        
+        for(int i=0; i<c.length; i++) {
+            
+            answer  = answer + (c[i] -'0');
+            
+        }
+     
+        answer = answer % 9;
+        
+        return answer;
+    }
+}
