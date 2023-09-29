a4b55b20ebcf5e6b05ded388da8110ceae1b4c49
sunghyun kim <105155263+kimsunghyun99@users.noreply.github.com>
2023-09-29 20:46:02 +0900

    [level 0] Title: 중복된 문자 제거, Time: 14.59 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+            HashSet<Character> set = new LinkedHashSet<>();
+        for(int i=0; i<my_string.length(); i++) {
+        set.add(my_string.charAt(i));
+            }
+        for(char c : set) {
+            answer += c; 
+        }
+        
+        return answer;
+    }
+}
