badbdfdf13b972b688b1db01c2124a31ff9aa39d
sunghyun kim <105155263+kimsunghyun99@users.noreply.github.com>
2023-10-17 14:13:12 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,40 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] cnt = new int[n];
-        for(int i=0; i<n; i++) {
-            cnt[i] = 1;
-        }
+      ArrayList<Integer> list = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+         list.add(1);
+      }
+      for (int i : reserve) {
+         list.set(i - 1, list.get(i-1)+1);
+      }
+      for (int i : lost) {
+         list.set(i - 1, list.get(i-1)-1);
+      }
         
-        for(int i=0; i<lost.length; i++) {
-            cnt[lost[i]-1]--;
-        }
         
-        for(int i=0; i<reserve.length; i++) {
-            cnt[reserve[i]-1]++;
-        }
-        
-        for(int i=0; i<n; i++) {
-            if(cnt[i] ==0) {
-                
-                if(i>0 && cnt[i-1]>1) {
-                    answer++;
-                    cnt[i-1]--;
-                    continue;
-                }
-                if(i<n-1 && cnt[i+1] >1) {
-                    answer++;
-                    cnt[i+1]--;
-                    continue;
-                }
+      for (int i = 0; i < n; i++) {
+         if (list.get(i) == 0) {
+            if (i >0 && list.get(i-1) == 2) {
+               list.set(i, 1);
+               list.set(i-1, 1);
+            answer++;
+                continue;
             }
-            else {
-                    answer++;
-                }
-           
+            if (i < n-1 && list.get(i+1) == 2) {
+               list.set(i, 1);
+               list.set(i+1, 1);
+                answer++;
+                continue;
+            }
+         } else {
+             answer++;
+         }
         }
-        
-        
-        return answer;
+     
+      return answer;
     }
 }
